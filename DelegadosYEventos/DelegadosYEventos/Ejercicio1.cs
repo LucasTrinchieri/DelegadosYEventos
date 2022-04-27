d75a604e3accb62f136ed99cbc55460b82c3fcb3
DAR-LAB-MA\UCSE <a>
2022-04-27 19:56:58 -0300

    Ejercicio 1

---
+++ DelegadosYEventos/DelegadosYEventos/Ejercicio1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegadosYEventos
+{
+    public class Persona
+    {
+        public EventHandler<NombreModificado> NombreModificado;
+
+        public delegate void actualizacionNombreCompletado(string nombre);
+
+        private string Nombre { get; set; }
+
+        public string ModificarNombre(string nombre)
+        {
+            string NombreViejo = Nombre;
+
+            Nombre = nombre;
+
+            this.NombreModificado(this, new NombreModificado()
+            {
+                NombreNuevo = nombre,
+                NombreViejo = NombreViejo
+            });
+
+            return Nombre;
+        }
+    }
+
+    public class NombreModificado : EventArgs
+    {
+        public string NombreNuevo { get; set; }
+        public string NombreViejo { get; set; }
+    }
+}
---
+++ DelegadosYEventos/MuestraEventos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DelegadosYEventos;
+
+namespace MuestraEventos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+
+            persona.NombreModificado += NombreModificadoHandler;
+
+            persona.ModificarNombre("Lucas");
+
+            Console.ReadKey();
+        }
+
+        static void NombreModificadoHandler(object sender, NombreModificado args)
+        {
+            Console.WriteLine($" NOmbre Nuevo: {args.NombreNuevo} y el NombreViejo: {args.NombreViejo}");
+        }
+    }
+}
